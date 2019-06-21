70f0b6ec5c9b3cd866095640a134a0ebd86996eb
cemreakyel <cemreakyel@sakarya.edu.tr>
2019-06-21 16:21:06 +0300

    Add project files.

---
+++ JSReportTestApp/Controllers/JSReportController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jsreport.AspNetCore;
+using jsreport.Types;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JSReportTestApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class JSReportController : Controller
+    {
+        private IJsReportMVCService _jsreportService;
+        public JSReportController(IJsReportMVCService jsreportService)
+        {
+            _jsreportService = jsreportService;
+        }
+        /// <summary>
+        /// pdf rapor görüntülemek için browser'a url'i giriniz
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var report = _jsreportService.RenderAsync(new RenderRequest()
+            {
+                Template = new Template
+                {
+                    Content = "<h1>Hello world -> {{name}}</h1>",
+                    Engine = Engine.Handlebars,
+                    Recipe = Recipe.ChromePdf
+                },
+                Data = new
+                {
+                    name = "Cemre"
+                }
+            }).Result;
+
+            return File(report.Content, report.Meta.ContentType);
+        }
+    }
+}
---
+++ JSReportTestApp/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using jsreport.AspNetCore;
+using jsreport.Binary;
+using jsreport.Local;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace JSReportTestApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddJsReport(new LocalReporting()
+                    .UseBinary(JsReportBinary.GetBinary())
+                    .AsUtility()
+                    .Create());
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Test Api",
+                    Version = "0.1.0",
+                    TermsOfService = "None"
+                });
+                //Set the comments path for the swagger json and ui.
+                var basePath = AppContext.BaseDirectory; //Depricated PlatformServices.Default.Application.ApplicationBasePath
+                var xmlPath = Path.Combine(basePath, "Test.Api.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test");
+            });
+        }
+    }
+}
